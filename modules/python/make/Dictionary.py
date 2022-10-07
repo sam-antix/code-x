9dfcfc8096b79c10112ca53a142cc23d53bf39b9
sam-antix <58833003+sam-antix@users.noreply.github.com>
2022-10-07 03:23:35 -0500

    So it begins...

---
+++ modules/python/make/Dictionary.py
@@ -0,0 +1,284 @@
+# Title: Dictionary.py
+# Date: 2022-10-06 20:49:04.000-05:00 (CST)
+
+# ----------------------------------------------------------
+class Dictionary(object):
+    '''
+    class for making, assessing, and modifying dictionary objects
+
+    @author: samantix
+
+    '''
+    # * Initializer ------------------------------------------------
+
+    def __init__(self, Dict):
+        self.Dict = Dict
+
+    # * Getters ----------------------------------------------------
+
+    def getDict(self):
+        return self.Dict
+
+    # * Tellers ----------------------------------------------------
+
+    def __repr__(self):
+        return 'Dictionary({})'.format(str(self.getDict())[1:-1])
+
+    def __str__(self):
+        '''
+        returns a str representation of argument (prioritized before __repr__() method)
+        '''
+        return str(self.getDict())
+
+    def isSuperSetOf_v1(self, d2):
+        '''
+        I: 2 dictionaries with int/float values
+        O: bool eval of d2 as subset of d1
+        Tech: all(), for in, items(), get(), values()
+        @author: samantix
+        '''
+        d1 = self.getDict()
+        d2 = d2.getDict()
+        new_d = {}
+        for k, v in d1.items():
+            new_d[k] = v - d2.get(k, 0)
+        return all(set(d2) <= set(d1) and i >= 0 for i in new_d.values())
+
+    def isSuperSetOf_v2(self, d2):
+        '''
+        Convert dicts to sets of their keys & verify the word is a subset of the hand
+
+        I: 2 dictionaries with int/float values
+        O: bool eval of d2 as subset of d1
+        F: True (or False)
+        Tech: all(), for in, items(), get(), values()
+        @author: samantix
+        Version: 2
+        '''
+        d1 = self.getDict()
+        d2 = d2.getDict()
+        if set(d2) <= set(d1):
+            # remove all char of the word from the hand
+            new_d = {k: d1[k] - d2.get(k, 0) for k, v in d1.items()}
+            # verify that the hand has no negative char values after word removal
+            return all(i >= 0 for i in new_d.values())
+        else:
+            return False
+
+    def isSuperSetOf(self, d2):
+        '''
+        Convert dicts to sets of their keys & verify the word is a subset of the hand
+
+        I: 2 dictionaries with int/float values
+        O: bool eval of d2 as subset of d1
+        Tech: all(), for in, items(), get(), values()
+        @author: samantix
+        Version: 3
+        '''
+        d1 = self.getDict()
+        d2 = d2.getDict()
+        # remove all char in word from hand
+        new_d = {k: d1[k] - d2.get(k, 0) for k, v in d1.items()}
+        # convert dicts to sets of their keys;
+        # verify word is subset of hand; and
+        # verify hand w/o neg char val after word removal
+        return all(set(d2) <= set(d1) and i >= 0 for i in new_d.values())
+
+    # * Doers ------------------------------------------------------
+
+    def gen_letters(self, case="l"):
+        '''
+        I: empty dictionary; case (u=upper, l=lower, a=all)
+        O: dict  
+        F: {ascii letter : int}
+        '''
+        # ----------------------------------------------------------
+        # *  Sub-methods
+
+        def upper_case():
+            return {chr(65+i): i for i in range(26)}
+
+        def lower_case():
+            return {chr(65+i): i for i in range(32, 58)}
+
+        def all_case():
+            return ({**upper_case(), **lower_case()})
+
+        # ----------------------------------------------------------
+        # * Option functions
+
+        def invalid():
+            print('Invalid command.\n')
+
+        def getLetters(case):
+
+            option = {
+                "u": ("upper-case", upper_case),
+                "l": ("lower-case letters", lower_case),
+                "a": ("upper- & lower-case letters", all_case)
+            }
+
+            return option.get(case, [None, invalid])[1]()
+        # ----------------------------------------------------------
+
+        return getLetters(case)
+        # ----------------------------------------------------------
+
+    def merge(self, d2={}):
+        '''
+        I: any 2 dicts
+        O: 1 merged dict
+        F: {k:v}
+        '''
+        return {**self.getDict(), **d2.getDict()}
+
+    def subtractVals_v1(self, d2):
+        '''
+        I: 2 dictionaries with int/float values
+        O: dict of d1 less d2
+        Tech: for in, items(), get()
+        @author: samantix
+        credit: https://stackoverflow.com/questions/17671875/how-to-subtract-values-from-dictionaries
+        '''
+        d1 = self.getDict()
+        d2 = d2.getDict()
+        new_d = {}
+        for k, v in d1.items():
+            new_d[k] = v - d2.get(k, 0)
+        return new_d
+
+    def subtractVals(self, d2):
+        '''
+        I: 2 dictionaries with int/float values
+        O: dict of d1 less d2
+        Tech: list comprehension, get(), for in, items()
+        @author: samantix
+        Version: 2
+        '''
+        d1 = self.getDict()
+        d2 = d2.getDict()
+        return {k: d1[k] - d2.get(k, 0) for k, v in d1.items()}
+
+    def swap_KeyVal(self):
+        '''
+        I: dict x1
+        O: dict x1
+        F: {k:v}
+        # * Ex: {k:v} -> {v:k} 
+        '''
+        d = self.getDict()
+        return {v: k for k, v in iter(d.items())}
+
+    def group(self):
+        '''
+        groups (itemizes) elements of a sequence
+
+        I: sequence (iterable): list,dict,str, etc.
+        O: dict
+        F: {item:item_count}
+        '''
+        d = {}
+        for e in self.getDict():
+            d[e] = d.get(e, 0) + 1
+        return d
+
+    def ungroup(self):
+        '''
+        I: dict of str:int pairs
+        O: list of concatenated strings of key-value products
+        F: {'a':2,'b':1,'z':3} -> a a b z z z
+        @author: samantix
+        DOC: 10/01/2022 19:00 CST
+        '''
+        d = self.getDict()
+        # Initialize list that will hold the key-value products
+        keyList = []
+        # unpack dict chars as if they were quantified by their respective values
+        for k, v in d.items():
+            keyList += str(k)*int(v)
+
+        # join them horizontally, separated by a single space
+        return ' '.join(keyList)
+
+    # ----------------------------------------------------------
+
+
+# @ --------------------------------------------- @
+# @                   TESTING                     @
+# @ --------------------------------------------- @
+
+
+# * gen_letters ----------------------------------------------------------
+# + ------------------WORKING-------------------- +
+
+d = Dictionary({})
+
+print('\n# Testing "gen_letters":')
+print('\nDefault:\n\n', d.gen_letters())
+print('\nInput "l":\n\n', d.gen_letters('l'))
+print('\nInput "u":\n\n', d.gen_letters('u'))
+print('\nInput "a":\n\n', d.gen_letters('a'))
+
+# * group ----------------------------------------------------------
+# + ------------------WORKING-------------------- +
+
+seq = Dictionary('pontchartrain')
+
+print('\n# Testing "group":\n')
+# print('# Before:\t', seq)
+# print('# After:\t', seq.group())
+print(seq, '\t->\t', seq.group())
+
+# * ungroup ----------------------------------------------------------
+# + ------------------WORKING-------------------- +
+
+d1 = Dictionary({'a': 2, 'b': 1, 'z': 3})
+
+print('\n# Testing "ungroup":\n')
+# print(d1.ungroup())  # expected: a a b z z z
+print(d1, '\t->\t', d1.ungroup())
+
+# * merge ----------------------------------------------------------
+# + ------------------WORKING-------------------- +
+
+d3 = Dictionary({"imagine": 1, "and": 2, "make": 3})
+d4 = Dictionary({"one": 4, "from": 5, "two": 6})
+
+print('\n# Testing "merge":\n')
+# print(d3.merge(d4))
+print(d3, '\t+\t', d4, '\t->\t', d3.merge(d4))
+
+# * substractVals --------------------------------------------------
+# + ------------------WORKING-------------------- +
+
+d5 = Dictionary({'h': 1, 'e': 1, 'l': 2, 'o': 1,
+                'a': 10, 'b': 9, 'c': 8, 'd': 7})
+d6 = Dictionary({'h': 1, 'e': 1, 'l': 1, 'o': 1})
+
+# Concise
+print('\n# Testing "subtractVals":\n')
+print(d5.subtractVals_v1(d6), '\t# v1')
+print(d5.subtractVals(d6), '\t# v2')
+
+# todo: clean up the format of the below test
+# Descriptive
+# print('# TECHNIQUE—1: for loop\n\t# Updated d1:\n\t', d5.subtractVals_v1(d6))
+# print('\n\t# d2 is subset of d1:\n\t', d5.subtractVals_v1(d6))
+# print('\n# TECHNIQUE—2: list comprehension\n\t# Updated d1:\n\t',
+#       d5.subtractVals(d6))
+# print('\n\t# d2 is subset of d1:\n\t', d5.subtractVals(d6))
+
+# * isSupersetOf ---------------------------------------------------
+# + ------------------WORKING-------------------- +
+
+print('\n# Testing "isSupersetOf":\n')
+print(f"{d5.isSuperSetOf_v1(d6)}\t# Expected:\t\tTrue\t# v1")
+print(f"{d5.isSuperSetOf_v2(d6)}\t# Expected:\t\tTrue\t# v2")
+print(f"{d5.isSuperSetOf(d6)}\t# Expected:\t\tTrue\t# v3")
+
+# * swap_KeyVal ----------------------------------------------------
+# + ------------------WORKING-------------------- +
+print('\n# Testing "swap_KeyVal":\n')
+print('# Before:\t', d1)
+print('# After:\t', d1.swap_KeyVal())
+#  ------------------END TEST---------------------
