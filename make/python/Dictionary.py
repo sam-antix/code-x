c10039b4e39d2ed7e15b1a3ba3690cde966195fd
sam-antix <58833003+sam-antix@users.noreply.github.com>
2022-10-12 18:53:55 -0500

    Dictionary: updated method names; added methods Loan: added methods, updated variable names

--- make/python/Dictionary.py
+++ make/python/Dictionary.py
@@ -1,30 +1,36 @@
 # Title: Dictionary.py
 # Date: 2022-10-06 20:49:04.000-05:00 (CST)
+# Encoding: UTF-8
 # @author: samantix
 
 # ----------------------------------------------------------
 class Dictionary(object):
     '''class for making, assessing, and modifying dictionary objects'''
     # * Initializer ------------------------------------------------
+    # initializes initial class object attributes
 
-    def __init__(self, Dict={}):
-        self.Dict = Dict
+    def __init__(self, d={}):
+        '''method called upon object instantiation (creation)'''
+        self.d = d
 
     # * Getters ----------------------------------------------------
 
-    def getDict(self):
-        return self.Dict
+    def get_d(self):
+        return self.d
 
     # * Tellers ----------------------------------------------------
 
     def __repr__(self):
-        return 'Dictionary({})'.format(str(self.getDict())[1:-1])
+        return 'Dictionary({})'.format(str(self.d)[1:-1])
 
     def __str__(self):
         '''
         returns a str representation of argument (prioritized before __repr__() method)
         '''
-        return str(self.getDict())
+        return str(self.get_d())
+
+    def __add__(self, d2):
+        return self.merge(d2)
 
     def isSuperSetOf_v1(self, d2):
         '''
@@ -33,8 +39,8 @@ class Dictionary(object):
         Tech: all(), for in, items(), get(), values()
         @author: samantix
         '''
-        d1 = self.getDict()
-        d2 = d2.getDict()
+        d1 = self.get_d()
+        d2 = d2.get_d()
         new_d = {}
         for k, v in d1.items():
             new_d[k] = v - d2.get(k, 0)
@@ -51,8 +57,8 @@ class Dictionary(object):
         @author: samantix
         Version: 2
         '''
-        d1 = self.getDict()
-        d2 = d2.getDict()
+        d1 = self.get_d()
+        d2 = d2.get_d()
         if set(d2) <= set(d1):
             # remove all char of the word from the hand
             new_d = {k: d1[k] - d2.get(k, 0) for k, v in d1.items()}
@@ -71,8 +77,8 @@ class Dictionary(object):
         @author: samantix
         Version: 3
         '''
-        d1 = self.getDict()
-        d2 = d2.getDict()
+        d1 = self.get_d()
+        d2 = d2.get_d()
         # remove all char in word from hand
         new_d = {k: d1[k] - d2.get(k, 0) for k, v in d1.items()}
         # convert dicts to sets of their keys;
@@ -85,66 +91,111 @@ class Dictionary(object):
     def make_dict(self, case="l"):
         '''
         I: empty dictionary; case (u=upper, l=lower, a=all)
-        O: dict  
+        O: d  
         F: {ascii letter : int}
         '''
         # ----------------------------------------------------------
-        # *  Sub-methods
+        # *  Sub-functions
         # Variables for improved readability
         ascii_letter_range = 65
         upper_case_letters = range(26)
-        lower_case_letter = range(32, 58)
+        lower_case_letters = range(32, 58)
 
         def upper_case():
             return {chr(ascii_letter_range + ucc_index): ucc_index for ucc_index in upper_case_letters}
 
         def lower_case():
-            return {chr(ascii_letter_range + lcc_index): lcc_index for lcc_index in lower_case_letter}
+            return {chr(ascii_letter_range + lcc_index): lcc_index for lcc_index in lower_case_letters}
 
-        def all_case():
+        def all_case_indexed():
             return ({**upper_case(), **lower_case()})
 
-        def paired_case():
-            return {chr(ascii_letter_range + ucc_index): [chr(ascii_letter_range + lcc_index) for lcc_index in lower_case_letter][ucc_index] for ucc_index in upper_case_letters}
+        def all_case_paired():
+            return {chr(ascii_letter_range + ucc_index): [chr(ascii_letter_range + lcc_index) for lcc_index in lower_case_letters][ucc_index] for ucc_index in upper_case_letters}
+        # ----------------------------------------------------------
         # ----------------------------------------------------------
         # * Option functions
 
         def invalid():
-            print('Invalid command.\n')
+            print('Invalid input.\n')
 
         def get_case(case):
 
             option = {
                 "l": ("Format: 'a':31,...'z':57 (default)", lower_case),
                 "u": ("Format: 'A':0,...'Z':25", upper_case),
-                "a": ("Format: 'A':0,...'z':57", all_case),
-                "p": ("Format: 'A':'a',...'Z':'z'", paired_case)
+                "ai": ("Format: 'A':0,...'z':57", all_case_indexed),
+                "ap": ("Format: 'A':'a',...'Z':'z'", all_case_paired)
             }
 
             return option.get(case, [None, invalid])[1]()
         # ----------------------------------------------------------
 
         return get_case(case)
-        # ----------------------------------------------------------
+#!----------------------------------------------------------
+
+    # + ------------------WORKING-------------------- +
+    def get_key_str(self, case='l'):
+        # return {chr(ascii_letter_range + ucc_index): [chr(ascii_letter_range + lcc_index) for lcc_index in lower_case_letters][ucc_index] for ucc_index in upper_case_letters}.keys()
+        # return self.make_dict().keys()
+        return ''.join([k for k, v in self.make_dict(case).items()])
+# +----------------------------------------------------------
 
     def merge(self, d2={}):
         '''
         I: any 2 dicts
-        O: 1 merged dict
+        O: 1 merged d
         F: {k:v}
         '''
-        return {**self.getDict(), **d2.getDict()}
+        return {**self, **d2}
+#!----------------------------------------------------------
+
+    # + ------------------WORKING-------------------- +
+    def shift_keys(self, shift=0):
+        ud_str = Dictionary().get_key_str('u')
+        ld_str = Dictionary().get_key_str('l')
+
+        ud_dict = {char: ud_str[(ud_str.index(char) + shift) % 26]
+                   for char in ud_str}
+        ld_dict = {char: ld_str[(ld_str.index(char) + shift) % 26]
+                   for char in ld_str}
+
+        return {**ud_dict, **ld_dict}
+
+
+#!----------------------------------------------------------
+        # def modHand(self, word):
+        #     hc = self.hand.copy()
+        #     # {k:v for (k,v) in iterable}
+        #     wd = {k: hc[k] for k in hc if hc[k]}
+        #     for e in word:
+        #         hc[e] -= 1
+        #     self.hand = {k: hc[k] for k in hc if hc[k]}
+        #     return True
+#!----------------------------------------------------------
+        # {char:      uc_str[(uc_str.index(char) + shift) % 26]       for char in uc_str}
+        # {char:      lc_str[(lc_str.index(char) + shift) % 26]       for char in lc_str}
+
+        # k1 = char
+        # k2 = char
+        # v1 = uc_str[(uc_str.index(char) + shift) % 26]
+        # v2 = lc_str[(lc_str.index(char) + shift) % 26]
+        # iterable1 = uc_str
+        # iterable2 = lc_str
+
+        # {k:v for v in iterable}
+# +----------------------------------------------------------
 
     def subtractVals_v1(self, d2):
         '''
         I: 2 dictionaries with int/float values
-        O: dict of d1 less d2
+        O: d of d1 less d2
         Tech: for in, items(), get()
         @author: samantix
         credit: https://stackoverflow.com/questions/17671875/how-to-subtract-values-from-dictionaries
         '''
-        d1 = self.getDict()
-        d2 = d2.getDict()
+        d1 = self.get_d()
+        d2 = d2.get_d()
         new_d = {}
         for k, v in d1.items():
             new_d[k] = v - d2.get(k, 0)
@@ -153,50 +204,50 @@ class Dictionary(object):
     def subtractVals(self, d2):
         '''
         I: 2 dictionaries with int/float values
-        O: dict of d1 less d2
+        O: d of d1 less d2
         Tech: list comprehension, get(), for in, items()
         @author: samantix
         Version: 2
         '''
-        d1 = self.getDict()
-        d2 = d2.getDict()
+        d1 = self.get_d()
+        d2 = d2.get_d()
         return {k: d1[k] - d2.get(k, 0) for k, v in d1.items()}
 
     def swap_KeyVal(self):
         '''
-        I: dict x1
-        O: dict x1
+        I: d x1
+        O: d x1
         F: {k:v}
         # * Ex: {k:v} -> {v:k} 
         '''
-        d = self.getDict()
+        d = self.get_d()
         return {v: k for k, v in iter(d.items())}
 
     def group(self):
         '''
         groups (itemizes) elements of a sequence
 
-        I: sequence (iterable): list,dict,str, etc.
-        O: dict
+        I: sequence (iterable): list,d,str, etc.
+        O: d
         F: {item:item_count}
         '''
         d = {}
-        for e in self.getDict():
+        for e in self.get_d():
             d[e] = d.get(e, 0) + 1
         return d
 
     def ungroup(self):
         '''
-        I: dict of str:int pairs
+        I: d of str:int pairs
         O: list of concatenated strings of key-value products
         F: {'a':2,'b':1,'z':3} -> a a b z z z
         @author: samantix
         DOC: 10/01/2022 19:00 CST
         '''
-        d = self.getDict()
+        d = self.get_d()
         # Initialize list that will hold the key-value products
         keyList = []
-        # unpack dict chars as if they were quantified by their respective values
+        # unpack d chars as if they were quantified by their respective values
         for k, v in d.items():
             keyList += str(k)*int(v)
 
@@ -204,5 +255,42 @@ class Dictionary(object):
         return ' '.join(keyList)
 
 
-d = Dictionary()
-print(d.make_dict('p'))
+# @ --------------------------------------------- @
+# @                   TESTING                     @
+# @ --------------------------------------------- @
+
+
+#ud = Dictionary().make_dict('u')
+#ld = Dictionary().make_dict('l')
+# d.get_key_str()
+
+
+# print(d.keys())
+# print({char ud[i+3]: ld[i+3] for i in range(len(ud))})
+
+
+# print(Dictionary().shift_keys(3))
+# print(Dictionary.mro())
+# print(Make.mro())
+#d = Dictionary({'a': 1})
+# print(d)
+# print(Dictionary().shift_keys(3))
+
+
+# print(d1.merge(d2))
+
+
+d1 = Dictionary().make_dict()
+d2 = Dictionary().make_dict('u')
+#print(Dictionary.merge(d1, d2))
+#xd = Dictionary(x)
+#zd = Dictionary(z)
+#print(Dictionary.merge(xd, zd))
+
+x = {'a': 32, 'b': 33, 'c': 34, 'd': 35, 'e': 36, 'f': 37, 'g': 38, 'h': 39, 'i': 40, 'j': 41, 'k': 42, 'l': 43, 'm': 44,
+     'n': 45, 'o': 46, 'p': 47, 'q': 48, 'r': 49, 's': 50, 't': 51, 'u': 52, 'v': 53, 'w': 54, 'x': 55, 'y': 56, 'z': 57}
+
+y = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12,
+     'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
+
+print(Dictionary.merge(x, y))
--- make/python/Loan.py
+++ make/python/Loan.py
@@ -10,6 +10,7 @@ import math
 
 
 class Loan(object):
+    # + ------------------WORKING-------------------- +
     def __init__(self, principal, annualInterestRate, monthlyPaymentRate):
         self.principal = principal
         self.annual_int_rate = annualInterestRate
@@ -18,13 +19,26 @@ class Loan(object):
         self.monthly_int_rate = self.annual_int_rate / self.annual_months
         self.month_of_pmt = 0
 
+    # @ ------------------UNTESTED------------------- @
     def get_balance(self):
         return self.calc_balance()
 
+    # @ ------------------UNTESTED------------------- @
     def __str__(self):
         # return "Remaining balance: " + str(self.calc_balance)
         return str(self.calc_balance())
 
+    # + ------------------WORKING-------------------- +
+    def round_up(self, x, factor=1):
+        res = math.ceil(x / factor) * factor
+        return res
+
+    # + ------------------WORKING-------------------- +
+    def round_down(self, x, factor=1):
+        res = math.floor(x / factor) * factor
+        return res
+
+    # @ ------------------UNTESTED------------------- @
     def update_balance(self, OLB, min_monthly_pmt):
         int_rate = self.monthly_int_rate
         TMP = min_monthly_pmt
@@ -32,6 +46,7 @@ class Loan(object):
         OLB = (OLB - TMP) + (OLB - TMP) * int_rate
         return OLB
 
+    # @ ------------------UNTESTED------------------- @
     def calc_balance(self):
         OLB = self.principal  # outstanding loan balance
         TMP_rate = self.monthly_pmt_rate
@@ -44,6 +59,28 @@ class Loan(object):
             #month += 1
         return round(OLB, 2)
 
+    # + ------------------WORKING-------------------- +
+    def amortize(self, years_of_payment=1):
+        '''
+        # * assumes: payments are factors of 10, interest is compounded monthly
+        # * returns: lowest estimated monthly payment (float); and total accrued interest (float)
+        '''
+        annualInterestRate = 0.2
+        P = self.principal
+        r = self.annual_int_rate
+        t = years_of_payment
+        n = 12                         # payments/month/year
+        # ----------------------------------------------------------
+        rP = r*P
+        nt = n*t
+        rn1 = 1+(r/n)
+        # Estimated monthly installment (EMI)
+        EMI = round(rP / (n*(1-(rn1)**(-nt))), 2)
+        total_accrued_interest = round(n*EMI*t-P, 2)
+        return EMI, total_accrued_interest
+
+# ----------------------------------------------------------
+    # ! -----------------ERRONEOUS------------------ !
     def amortize_10(self, required_months_of_payment=12):
         # compounded
         P = self.principal
@@ -66,39 +103,89 @@ class Loan(object):
             dP = round(dP, 2)
         return EMI
 
-        #print("Lowest Payment: " + str(EMI))
-# ----------------------------------------------------------
-    def amortize(self, years_of_payment):
-        annualInterestRate = 0.2
-        P = self.principal
-        r = self.annual_int_rate
-        t = years_of_payment
-        n = 12                         # payments/month/year
-        # ----------------------------------------------------------
-        rP = r*P
-        nt = n*t
-        rn1 = 1+(r/n)
-        # Estimated monthly installment (EMI)
-        EMI = round(rP / (n*(1-(rn1)**(-nt))), 2)
-        total_accrued_interest = round(n*EMI*t-P, 2)
-        # ----------------------------------------------------------
-        print(f"Original principal: ${P}")
-        print(f"Min monthly payment: ${EMI}")
-        print(f"Total accrued interest: ${total_accrued_interest}")
-        print(f"Total to be paid: ${EMI * n}")
-# ----------------------------------------------------------
+    # + ------------------WORKING-------------------- +
 
+    def amortize_10x(self, term=1):
+        '''
+        # * assumes: payments are factors of 10, interest is compounded monthly
+        # * returns: lowest fixed monthly payment to clear loan balance
+        '''
+        total_installments = term * 12
+        monthly_int_rate = self.annual_int_rate / 12
+        # start at a reasonable estimated monthly installment (EMI)
+        x = self.principal / total_installments
+        # assume payments must be a factor of 10
+        EMI = self.round_down(x, 10)
+        if self.principal - EMI*10 > 0:
+            updated_balance = self.principal - EMI
+            while updated_balance > 0:
+                EMI += 10
+                updated_balance = self.principal
+                for installment in range(total_installments):
+                    updated_balance -= EMI
+                    # add interest (monthly_int_rate * updated_balance)
+                    updated_balance += monthly_int_rate * updated_balance
+                updated_balance = round(updated_balance, 2)
+        total_accrued_interest = round(12*EMI*term-self.principal, 2)
+        return EMI, total_accrued_interest
+
+# ----------------------------------------------------------
+    # + ------------------WORKING-------------------- +
+    def get_amortization(self, total_installments=12):
+        EMI, total_interest = self.amortize()
+        print("Amortization method: standard formula")
+        print(f"Original principal: ${self.principal}")
+        print(f"Min monthly payment: ${EMI}")
+        print(f"Total accrued interest: ${total_interest}")
+        print(f"Total to be paid: ${EMI * total_installments}")
+
+# ----------------------------------------------------------
+    # + ------------------WORKING-------------------- +
+    def get_amortization_10x(self, total_installments=12):
+        EMI, total_interest = self.amortize_10x()
+        print("Amortization method: factor 10")
+        print(f"Original principal: ${self.principal}")
+        print(f"Min monthly payment: ${EMI}")
+        print(f"Total accrued interest: ${total_interest}")
+        print(f"Total to be paid: ${EMI * total_installments}")
+# ----------------------------------------------------------
+    # + ------------------WORKING-------------------- +
+
+    def get_min_amortization(self):
+        EMI_a, total_interest_a = self.amortize()
+        EMI_b, total_interest_b = self.amortize_10x()
+        if EMI_a <= EMI_b:
+            print("Best amortization: amortize")
+            return self.get_amortization()
+        else:
+            print("Best amortization: amortize_10x")
+            return self.get_amortization_10x()
+# ----------------------------------------------------------
 # @ --------------------------------------------- @
 # @                   TESTING                     @
 # @ --------------------------------------------- @
 
 
-principal = 3329
-annualInterestRate = 0.2
+principal = 619  # 4800
+annualInterestRate = 0.18  # 0.15
 monthlyPaymentRate = 0.04
 term = 1
+total_installments = 12
+# ----------------------------------------------------------
+print("\n***Testing 'get_amortization' & 'amortize'***\n")
+Loan(principal, annualInterestRate,
+     monthlyPaymentRate).get_amortization(total_installments)
 
-print("\n***Testing 'amortize'***\n")
-Loan(principal, annualInterestRate, monthlyPaymentRate).amortize(term)
+print("\n***Testing 'get_amortization_10x' & 'amortize_10x'***\n")
+
+Loan(principal, annualInterestRate,
+     monthlyPaymentRate).get_amortization_10x(total_installments)
+
+print("\n***Testing 'get_min_amortization'***\n")
+
+Loan(principal, annualInterestRate,
+     monthlyPaymentRate).get_min_amortization()
+
 print("\n***Testing 'amortize_10'***\n")
+
 print(Loan(principal, annualInterestRate, monthlyPaymentRate).amortize_10())
